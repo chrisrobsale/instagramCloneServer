4f4dc2478751a496109f4345a64a1a1a8e5a0fa4
dimplejanebondoc <dimple.bondoc@appetiser.com.au>
2023-05-08 16:59:26 +0800

    installed and implemented cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = 5000
+const cors = require('cors')
+const PORT = 4000
 
 //connect to db
 const mongoose = require('./db/connect')
 
+app.use(cors())
 app.use(express.json()) //parsing request body
 app.use(require('./routes/auth')) 
 app.use(require('./routes/post')) 
--- routes/auth.js
+++ routes/auth.js
@@ -7,45 +7,45 @@ const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const requireLogin = require('../middlewares/requireLogin')
 
-router.get('/', (req,res) => {
-    async function retryRequest(promiseFunc, nrOfRetries) {
-        // Write your code here
-        let response;
-        for(i=1;i<=nrOfRetries;i++){
-            console.log(i)
-            let retval = await promiseFunc().then(res => {
-                return res;
-            }).catch((error) => {
-                return false;
-            })
-            if(retval){
-                response = retval
-                i = nrOfRetries + 1
-            }else{
-                response = "Exception!"
-            }
-        }
-        return response;
-      }
+// router.get('/', (req,res) => {
+//     async function retryRequest(promiseFunc, nrOfRetries) {
+//         // Write your code here
+//         let response;
+//         for(i=1;i<=nrOfRetries;i++){
+//             console.log(i)
+//             let retval = await promiseFunc().then(res => {
+//                 return res;
+//             }).catch((error) => {
+//                 return false;
+//             })
+//             if(retval){
+//                 response = retval
+//                 i = nrOfRetries + 1
+//             }else{
+//                 response = "Exception!"
+//             }
+//         }
+//         return response;
+//       }
               
-      let hasFailed = false;
-      function getUserInfo() {
-        return new Promise((resolve, reject) => {
-          if(!hasFailed) {
-            hasFailed = true;
-            reject("Exception!");
-          } else {
-            resolve("Fetched user!");
-          }
-        });
-      }
-      let promise = retryRequest(getUserInfo, 3);
-      if(promise) {
-        promise.then((result) => console.log(result))
-        .catch((error) => console.log("Error!"));
-      }
-    res.send("hello isaw")
-})
+//       let hasFailed = false;
+//       function getUserInfo() {
+//         return new Promise((resolve, reject) => {
+//           if(!hasFailed) {
+//             hasFailed = true;
+//             reject("Exception!");
+//           } else {
+//             resolve("Fetched user!");
+//           }
+//         });
+//       }
+//       let promise = retryRequest(getUserInfo, 3);
+//       if(promise) {
+//         promise.then((result) => console.log(result))
+//         .catch((error) => console.log("Error!"));
+//       }
+//     res.send("hello isaw")
+// })
 
 router.get('/protected', requireLogin, (req,res) => {
   res.send("test")
@@ -55,52 +55,54 @@ router.post('/signup', (req,res) => {
     const {name, email, password} = req.body
     if(!email || !password || !name){
       res.status(400).send({error: "Invalid Request"})
-    }
+    }else{
+      User.findOne({email:email})
+        .then((savedUser) => {
+          if(savedUser){
+            res.status(400).send({error: "User already exists"})
+          }
+          bcrypt.hash(password,12).then(hashpass => {
+            const newUser = new User({
+              email,
+              password:hashpass,
+              name
+            })
     
-    User.findOne({email:email})
-      .then((savedUser) => {
-        if(savedUser){
-          return res.status(400).send({error: "User already exists"})
-        }
-        bcrypt.hash(password,12).then(hashpass => {
-          const newUser = new User({
-            email,
-            password:hashpass,
-            name
+            newUser.save()
+              .then(u => {
+                res.status(201).send({message: "User saved successfully"})
+              })
+              .catch(e => {
+                res.status(500).send({error: e.message})
+              })
+          }).catch(e => {
+            res.status(500).send({error: e.message})
           })
-  
-          newUser.save()
-            .then(u => {
-              res.status(201).send({message: "User saved successfully"})
-            })
-            .catch(e => {
-              res.status(500).send({error: e.message})
-            })
-        }).catch(e => {
+          
+        })
+        .catch(e =>{
           res.status(500).send({error: e.message})
         })
-        
-      })
-      .catch(e =>{
-        res.status(500).send({error: e.message})
-      })
+    }
 })
 
 router.post('/signin', (req,res) => {
   const {email, password} = req.body
   if(!email || !password) res.status(400).send({error: "Invalid Username/Password"})
-  User.findOne({email:email}).then(savedUser => {
-    if(!savedUser) res.status(400).send({error: "Invalid Username/Password"})
-    bcrypt.compare(password, savedUser.password).then(doMatch => {
-      if(doMatch) {
-        const token = jwt.sign({_id: savedUser._id,},process.env.JWT_SECRET)
-        res.status(200).send({token})
-      }
-      else res.status(400).send({error: "Invalid Username/Password"})
-    }).catch(e => {
-      res.status(500).send({error: e.message})
+  else {
+    User.findOne({email:email}).then(savedUser => {
+      if(!savedUser) res.status(400).send({error: "Invalid Username/Password"})
+      bcrypt.compare(password, savedUser.password).then(doMatch => {
+        if(doMatch) {
+          const token = jwt.sign({_id: savedUser._id,},process.env.JWT_SECRET)
+          res.status(200).send({token})
+        }
+        else res.status(400).send({error: "Invalid Username/Password"})
+      }).catch(e => {
+        res.status(500).send({error: e.message})
+      })
     })
-  })
+  }
 })
 
 module.exports = router
